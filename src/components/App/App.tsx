236f93fdb67431da9e5696a2b92af11f48da7d61
helen-akateva <akareva17@gmail.com>
2025-10-03 14:20:35 +0300

    update

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import css from './App.module.css';
+
 import CafeInfo from '../CafeInfo/CafeInfo'
 
 export default function App() {
+    const handleVote(type)
+
+const resetVotes()
+    
  return <div className={css.app}><CafeInfo/></div>
 
 }
