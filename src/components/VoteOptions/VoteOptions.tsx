9e7eb15b6cf8f91241b5502a2be530b33e45dab4
helen-akateva <akareva17@gmail.com>
2025-10-03 19:10:25 +0300

    update

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,11 +1,18 @@
 import css from './VoteOptions.module.css'
+import type {VoteType} from '../../types/votes';
 
-export default function VoteOptions() {
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({onVote, onReset}: VoteOptionsProps) {
   return <div className={css.container}>
-  <button className={css.button}>Good</button>
-  <button className={css.button}>Neutral</button>
-  <button className={css.button}>Bad</button>
-  <button className={`${css.button} ${css.reset}`}>Reset</button>
+  <button onClick={() => onVote("good")}className={css.button}>Good</button>
+  <button onClick={() => onVote("neutral")}className={css.button}>Neutral</button>
+  <button onClick={() => onVote("bad")}className={css.button}>Bad</button>
+  <button onClick={() => onReset()} className={`${css.button} ${css.reset}`}>Reset</button>
 </div>
 
 }
--- src/components/VoteStats/VoteStats.tsx
+++ src/components/VoteStats/VoteStats.tsx
@@ -1,7 +1,17 @@
-<div className={styles.container}>
-  <p className={styles.stat}>Good: <strong>0</strong></p>
-  <p className={styles.stat}>Neutral: <strong>0</strong></p>
-  <p className={styles.stat}>Bad: <strong>0</strong></p>
-  <p className={styles.stat}>Total: <strong>0</strong></p>
-  <p className={styles.stat}>Positive: <strong>0%</strong></p>
-</div>
+import styles from './VoteStats.module.css'
+import type {Votes} from '../../types/votes'
+
+interface VoteStatsProps {
+  votes: Votes;
+  totalVotes: number;
+  positiveRate: number;
+}
+
+export default function VoteStats({ votes, totalVotes, positiveRate }: VoteStatsProps) {
+  return <div className={styles.container}>
+    <p className={styles.stat}>Good: <strong>{votes.good}</strong></p>
+    <p className={styles.stat}>Neutral: <strong>{ votes.neutral}</strong></p>
+    <p className={styles.stat}>Bad: <strong>{votes.neutral }</strong></p>
+    <p className={styles.stat}>Total: <strong>{totalVotes }</strong></p>
+    <p className={styles.stat}>Positive: <strong>{positiveRate }</strong></p>
+</div >}
